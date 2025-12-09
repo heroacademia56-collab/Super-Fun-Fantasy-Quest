63d394889e75097b2c4a2187a176d7d7420adfbc
HeroA <heroacademia56@gmail.com>
2025-12-08 21:27:22 -0600

    Add initial project.py with main and core functions

---
+++ src/project.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome to Quest for the Sword of Power!")
+
+def intro():
+    print("This is the story intro...")
+
+def choice():
+    print("You must choose your path...")
+
+def outcome():
+    print("This is what happens after your choice.")
+
+if __name__ == "__main__":
+    main()
